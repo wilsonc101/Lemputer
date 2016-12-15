8dbb7fd4f287757c57663b1327ef4943501a5cdb
root <wilsonc101@gmail.com>
2016-12-15 22:50:17 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+A basic website based on AWS Lambda with Python.
+
+Configuration performed by Amazon Chalice and renders a PyGal graph as an embedded HTML element.
---
+++ lambda/lemputer/app.py
@@ -0,0 +1,21 @@
+from flask import Markup
+import pygal
+
+from chalice import Chalice
+
+app = Chalice(app_name='lemputer')
+
+
+line_chart = pygal.Line(width=500, height=400)
+line_chart.add('A', [1, 3,  5, 16, 13, 3,  7])
+
+graph = line_chart.render_data_uri()
+
+test_html = """    <div id="graph_panel">
+      <embed type="image/svg+xml" src={0} width=600 height=500>
+    </div>""".format(graph)
+
+
+@app.route('/')
+def index():
+    return {'title': 'Chalice with PyGal', 'body': test_html}
